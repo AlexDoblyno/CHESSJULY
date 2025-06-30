9faa2f03e28199c6954e2781feedbe1c28978d6b
AlexDoblyno <alexdoblyno@gmail.com>
2025-06-29 22:53:09 -0600

    更改QUEENmoves

--- shared/src/main/java/chess/piecemoves/KingMoves.java
+++ shared/src/main/java/chess/piecemoves/KingMoves.java
@@ -1,21 +1,20 @@
 package chess.piecemoves;
 
 import chess.ChessBoard;
-import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class KingMoves extends PieceMoves {
-    public KingMoves(ChessBoard gameBoard, ChessPosition StartPosition) {
-        super(gameBoard, StartPosition);
+    public KingMoves(ChessBoard GameBoard, ChessPosition StartPosition) {
+        super(GameBoard, StartPosition);
         calculateMoves();
     }
 
     @Override
     public void calculateMoves() {
         ChessPosition checkPosition;
-        for (int row = StartPosition.getRow() - 2; row <= StartPosition.getRow() + 2; row++) {
-            for (int col = StartPosition.getColumn() - 2; col <= StartPosition.getColumn() + 2; col++) {
-                if (Math.abs(StartPosition.getRow() - row) + Math.abs(StartPosition.getColumn() - col) == 3 && isInBounds(row, col)) {
+        for (int row = StartPosition.getRow() - 1; row <= StartPosition.getRow() + 1; row++) {
+            for (int col = StartPosition.getColumn() - 1; col <= StartPosition.getColumn() + 1; col++) {
+                if(isInBounds(row, col)) {
                     checkPosition = new ChessPosition(row, col);
                     checkSpace(checkPosition);
                 }
