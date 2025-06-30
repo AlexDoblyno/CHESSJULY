ff97db79b00da4ddefd21768e8f36eb4cded1051
AlexDoblyno <alexdoblyno@gmail.com>
2025-06-29 22:22:52 -0600

    更改kingmoves

--- shared/src/main/java/chess/piecemoves/KingMoves.java
+++ shared/src/main/java/chess/piecemoves/KingMoves.java
@@ -4,18 +4,18 @@ import chess.ChessBoard;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-public class KnightMoves extends PieceMoves {
-    public KingMoves(ChessBoard gameBoard, ChessPosition startPosition) {
-        super(gameBoard, startPosition);
+public class KingMoves extends PieceMoves {
+    public KingMoves(ChessBoard gameBoard, ChessPosition StartPosition) {
+        super(gameBoard, StartPosition);
         calculateMoves();
     }
 
     @Override
     public void calculateMoves() {
         ChessPosition checkPosition;
-        for (int row = StartPosition.getRow() - 2; row <= startPosition.getRow() + 2; row++) {
-            for (int col = StartPosition.getColumn() - 2; col <= startPosition.getColumn() + 2; col++) {
-                if (Math.abs(StartPosition.getRow() - row) + Math.abs(startPosition.getColumn() - col) == 3 && isInBounds(row, col)) {
+        for (int row = StartPosition.getRow() - 2; row <= StartPosition.getRow() + 2; row++) {
+            for (int col = StartPosition.getColumn() - 2; col <= StartPosition.getColumn() + 2; col++) {
+                if (Math.abs(StartPosition.getRow() - row) + Math.abs(StartPosition.getColumn() - col) == 3 && isInBounds(row, col)) {
                     checkPosition = new ChessPosition(row, col);
                     checkSpace(checkPosition);
                 }
