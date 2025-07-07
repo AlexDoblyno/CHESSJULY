88faf78c4de76debf6c6f50bd33cc51cb2f749ce
AlexDoblyno <alexdoblyno@gmail.com>
2025-07-07 14:48:45 -0600

    王车易位

--- shared/src/main/java/chess/CastlingHistory.java
+++ shared/src/main/java/chess/CastlingHistory.java
@@ -1,4 +1,17 @@
 package chess;
 
 public class CastlingHistory {
+    boolean WHITEKingRookMoved;
+    boolean WHITEQueenRookMoved;
+    boolean BLACKKingRookMoved;
+    boolean BLACKQueenRookMoved;
+    boolean WHITEKingMoved;
+    boolean BLACKKingMoved;
 }
+    CastlingHistory(){
+        WHITEKingMoved = false;
+        BLACKKingMoved = false;
+        WHITEKingRookMoved = false;
+        BLACKKingRookMoved = false;
+
+    }
