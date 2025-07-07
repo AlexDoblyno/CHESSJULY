7da3ffbb55a104ca00c84d4c20152bd7d40f0e4c
AlexDoblyno <alexdoblyno@gmail.com>
2025-07-07 02:49:45 -0600

    编写ChessGame Checkmate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -26,8 +26,8 @@ public class ChessGame {
         GameBoard.resetBoard();
         previousMove = null;
         castlingHistory = new CastlingHistory();
-        checkStalemate = new CheckStalemate(GameBoard);
-        inCheckDeterminer = new InCheckDeterminer(gameBoard);
+        checkStalemate = new CheckStalemate(GameBoard); //没有编写导致报错
+        inCheckDeterminer = new InCheckDeterminer(GameBoard);
     }
 
     /**
@@ -64,7 +64,7 @@ public class ChessGame {
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
         Collection<ChessMove> pieceMoves;
         if (GameBoard.getPiece(startPosition) != null) {
-            pieceMoves = GameBoard.getPiece(startPosition).pieceMoves(gameBoard, startPosition);
+            pieceMoves = GameBoard.getPiece(startPosition).pieceMoves(GameBoard, startPosition);
             pieceMoves.removeIf(this::testMove);
             if (previousMove != null) {
                 /// inster ENPASSENT///
@@ -84,50 +84,107 @@ public class ChessGame {
     /**
      * Determines if the given team is in check
      *
-     * @param teamColor which team to check for check
+     * @param TeamColor which team to check for check
      * @return True if the specified team is in check
      */
-    public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
-    }
+        public boolean isInCheck(TeamColor TeamColor) {
+            inCheckDeterminer.setGameBoard(GameBoard);
+            return inCheckDeterminer.isInCheck(TeamColor);
+        }
 
     /**
      * Determines if the given team is in checkmate
      *
-     * @param teamColor which team to check for checkmate
+     * @param TeamColor which team to check for checkmate
      * @return True if the specified team is in checkmate
      */
-    public boolean isInCheckmate(TeamColor TeamColor) {
-            inCheckDeterminer.setGameBoard(GameBoard);
-            return inCheckDeterminer.isInCheck(TeamColor);
-    }
+        public boolean isInCheckmate(TeamColor TeamColor){
+            if (!isInCheck(TeamColor)) {
+                return false;
+            }
+            else {
+                for (ChessMove move : getAllTeamMoves(TeamColor)) {
+                    if (!testMove(move)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
 
     /**
      * Determines if the given team is in stalemate, which here is defined as having
      * no valid moves while not in check.
      *
-     * @param teamColor which team to check for stalemate
+     * @param TeamColor which team to check for stalemate
      * @return True if the specified team is in stalemate, otherwise false
      */
-    public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
-    }
+        private Collection<ChessMove> getAllTeamMoves(TeamColor TeamColor) {
+            Collection<ChessMove> allMoves = new HashSet<>();
+            ChessPosition checkPosition;
+            ChessPiece checkPiece;
+            for (int row = 1; row <= 8; row++) {
+                for (int col = 1; col <= 8; col++) {
+                    checkPosition = new ChessPosition(row, col);
+                    checkPiece = GameBoard.getPiece(checkPosition);
+                    if (checkPiece != null && checkPiece.getTeamColor() == TeamColor) {
+                        allMoves.addAll(validMoves(checkPosition));
+                    }
+                }
+            }
+            return allMoves;
+        }
 
+        public boolean isInStalemate(TeamColor TeamColor) {
+            checkStalemate.setGameBoard(GameBoard);
+            return checkStalemate.isInStalemate(isInCheck(TeamColor), getAllTeamMoves(TeamColor));
+        }
     /**
      * Sets this game's chessboard with a given board
      *
      * @param board the new board to use
      */
-    public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
-    }
-
+        public void setBoard(ChessBoard board) {
+            ChessPosition setPosition;
+            ChessPiece setPiece;
+            castlingHistory.resetHistory();
+            for (int i = 1; i <= 8; i++) {
+                for (int j = 1; j <= 8; j++) {
+                    setPosition = new ChessPosition(i, j);
+                    if (board.getPiece(setPosition) != null) {
+                        setPiece = new ChessPiece(board.getPiece(setPosition).getTeamColor(), board.getPiece(setPosition).getPieceType());
+                        GameBoard.addPiece(setPosition, setPiece);
+                    }
+                    else {
+                        GameBoard.addPiece(setPosition, null);
+                    }
+                }
+            }
+        }
     /**
      * Gets the current chessboard
      *
      * @return the chessboard
      */
-    public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        public ChessBoard getBoard() {
+            return GameBoard;
+        }
+
+
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof ChessGame chessGame)) {
+                return false;
+            }
+            return currentTeam == chessGame.currentTeam && Objects.equals(GameBoard, chessGame.GameBoard);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(currentTeam, GameBoard);
+        }
     }
-}
