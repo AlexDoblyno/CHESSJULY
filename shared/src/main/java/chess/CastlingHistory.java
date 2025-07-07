de308deaed3b4fc2a106106e081f0af2b61f854b
AlexDoblyno <alexdoblyno@gmail.com>
2025-07-07 14:59:34 -0600

    王车易位

--- shared/src/main/java/chess/CastlingHistory.java
+++ shared/src/main/java/chess/CastlingHistory.java
@@ -7,11 +7,15 @@ public class CastlingHistory {
     boolean BLACKQueenRookMoved;
     boolean WHITEKingMoved;
     boolean BLACKKingMoved;
-}
+
     CastlingHistory(){
         WHITEKingMoved = false;
         BLACKKingMoved = false;
         WHITEKingRookMoved = false;
         BLACKKingRookMoved = false;
-
+        WHITEQueenRookMoved = false;
+        BLACKQueenRookMoved = false;
+    } //王车易位前，检测王与车有无移动，若是先前移动，无法王车易位
+    public void resetHistory(){
     }
+}
